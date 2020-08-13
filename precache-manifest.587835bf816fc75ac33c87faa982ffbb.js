3344448e279d5d31b63e38b161ae38fd0735fa9f
Sebastian <sebasmarro10@gmail.com>
2020-08-13 14:04:52 -0500

    Updates

---
+++ precache-manifest.587835bf816fc75ac33c87faa982ffbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b60f30cdc7a2ec8fee425ec40e7164",
+    "url": "/recipe-reactapp/index.html"
+  },
+  {
+    "revision": "c12bff6d456e58278360",
+    "url": "/recipe-reactapp/static/css/main.864d053a.chunk.css"
+  },
+  {
+    "revision": "2d7cada010bd3a8d1967",
+    "url": "/recipe-reactapp/static/js/2.bd14934e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-reactapp/static/js/2.bd14934e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12bff6d456e58278360",
+    "url": "/recipe-reactapp/static/js/main.4394d1e9.chunk.js"
+  },
+  {
+    "revision": "c1b8a9af7485604b9fa7",
+    "url": "/recipe-reactapp/static/js/runtime-main.c70ced27.js"
+  }
+]);
